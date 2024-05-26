b65d380fa1fef4cd38a95a9868ae21abbaf7d648
Piyush289kumar <piyushraikwar289@gmail.com>
2024-05-26 23:08:21 +0530

    "Added a commented out MySQL connection string to config.php"

--- config.php
+++ config.php
@@ -2,3 +2,4 @@
 $website_display_default_name = "Travel Blog";
 $hostname = "http://localhost/travel_blog";
 $conn = mysqli_connect('localhost', 'root', '', 'travel_blog_db') or die("Connection Failed!!" . mysqli_connect_error());
+// $conn = mysqli_connect('localhost', 'u423777452_travel_blog_db', 'Piyush@289', 'u423777452_travel_blog') or die("Connection Failed!!" . mysqli_connect_error());
